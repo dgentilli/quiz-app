c962a3520be6ffbe71b5b949c66aa29a0de7f53f
Dave Gentilli <dgentilli@gmail.com>
2021-06-11 16:32:34 +0300

    Start game, fetch questions, and implement conditional logic for displaying buttons.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,42 @@
 import React, { useState } from 'react';
 import QuestionCard from './components/QuestionCard';
-import { fetchQuestions, Difficulty } from './api';
+import { fetchQuestions, Difficulty, QuestionState } from './api';
 // here is the link for the trivia api
 // https://opentdb.com/api.php?amount=20&category=18&type=multiple
 
 /**
- * Left of at 38min 30 sec in the video
+ * Left of at 48min 25 sec in the video https://www.youtube.com/watch?v=F2JCjVSZlG0
  */
 
 const TOTAL_QUESTIONS = 20;
 
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+
 const App = () => {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
-  console.log(
-    'fetchQuestions',
-    fetchQuestions(TOTAL_QUESTIONS, Difficulty.EASY)
-  );
+  const startGame = async () => {
+    setLoading(true);
+    setGameOver(false);
 
-  const startGame = async () => {};
+    const newQuestions = await fetchQuestions(TOTAL_QUESTIONS, Difficulty.EASY);
+    //TO DO: Add try / catch block for error handling. Probably do this in the api file.
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
 
@@ -32,20 +45,30 @@ const App = () => {
   return (
     <div className='App'>
       <h1>Trivia Game</h1>
-      <button className='start' onClick={startGame}>
-        Start
-      </button>
-      <p className='score'>Score</p>
-      <p className='score'>Loading Questions ....</p>
-      {/* <QuestionCard 
-      questionNr={number + 1}
-      totalQuestions={ TOTAL_QUESTIONS}
-      question={questions[number].question}
-      answers={questions[number].answers}
-      userAnswer={userAnswers ? userAnswers[number] : undefined}
-      callback={checkAnswer}
-      /> */}
-      <button onClick={nextQuestion}>Next Question</button>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className='start' onClick={startGame}>
+          Start
+        </button>
+      ) : null}
+
+      {!gameOver ? <p className='score'>Score</p> : null}
+      {loading ? <p className='score'>Loading Questions ....</p> : null}
+      {!loading && !gameOver ? (
+        <QuestionCard
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      ) : null}
+      {!loading &&
+      !gameOver &&
+      userAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button onClick={nextQuestion}>Next Question</button>
+      ) : null}
     </div>
   );
 };
