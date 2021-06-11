9acf37497086b160caf887fdac545a705c2fe5b6
Dave Gentilli <dgentilli@gmail.com>
2021-06-11 09:37:44 +0300

    Implement state. Test API response. Temporarily comment out QuestionCard to test API.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,53 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import QuestionCard from './components/QuestionCard';
+import { fetchQuestions, Difficulty } from './api';
 // here is the link for the trivia api
 // https://opentdb.com/api.php?amount=20&category=18&type=multiple
+
+/**
+ * Left of at 38min 30 sec in the video
+ */
+
+const TOTAL_QUESTIONS = 20;
+
 const App = () => {
-  return <div className='App'>Quiz</div>;
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(
+    'fetchQuestions',
+    fetchQuestions(TOTAL_QUESTIONS, Difficulty.EASY)
+  );
+
+  const startGame = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+  return (
+    <div className='App'>
+      <h1>Trivia Game</h1>
+      <button className='start' onClick={startGame}>
+        Start
+      </button>
+      <p className='score'>Score</p>
+      <p className='score'>Loading Questions ....</p>
+      {/* <QuestionCard 
+      questionNr={number + 1}
+      totalQuestions={ TOTAL_QUESTIONS}
+      question={questions[number].question}
+      answers={questions[number].answers}
+      userAnswer={userAnswers ? userAnswers[number] : undefined}
+      callback={checkAnswer}
+      /> */}
+      <button onClick={nextQuestion}>Next Question</button>
+    </div>
+  );
 };
 
 export default App;
